60620456d99b83a7f24069c3752e33dbea19862b
Jean <jean.benitez@mawesi.net>
2024-05-23 22:21:38 -0500

    Commit 4

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ from gerente import Gerente
 from datos import datos_sistemas, datos_mercadeo, datos_ventas
 
 def main():
-    mercadeo = Departamento('Departamento de Mercadeo', Gerente('Julieta', 'Madolnado', 13*1300000))
+    mercadeo = Departamento('Departamento de Mercadeo', Gerente('Juana', 'Piedraita', 13*1300000))
     mercadeo.agregar_empleados(datos_mercadeo)
 
-    ventas = Departamento('Departamento de Ventas', Gerente('Guillermo', 'Palomino Caicedo', 13*1300000))
+    ventas = Departamento('Departamento de Ventas', Gerente('Carlos', 'Villgran Morales', 13*1300000))
     ventas.agregar_empleados(datos_ventas)
 
-    sistemas = Departamento('Departamento de Sistemas', Gerente('Margarita', 'Lopez Salinas', 13*1300000))
+    sistemas = Departamento('Departamento de Sistemas', Gerente('Fulanito', 'Perenseo Morenseo', 13*1300000))
     sistemas.agregar_empleados(datos_sistemas)
 
-    empresa = Empresa('Empresa XYZ', [mercadeo, ventas, sistemas])
+    empresa = Empresa('Empresa El Gato que esta triete y azul', [mercadeo, ventas, sistemas])
 
     empresa.mostrar()
 
